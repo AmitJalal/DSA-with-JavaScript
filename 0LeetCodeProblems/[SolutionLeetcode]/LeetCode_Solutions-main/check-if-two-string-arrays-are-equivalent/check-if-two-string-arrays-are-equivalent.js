fc39eef92ec27f9529df7606a38d5b9eb45c409b
AmitJalal <amitjalal53@gmail.com>
2023-03-21 08:22:55 +0530

    some recent codes

--- 0LeetCodeProblems/[SolutionLeetcode]/LeetCode_Solutions-main/check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.js
+++
@@ -1,8 +0,0 @@
-/**
- * @param {string[]} word1
- * @param {string[]} word2
- * @return {boolean}
- */
-var arrayStringsAreEqual = function(word1, word2) {
-    return word1.join('') === word2.join('')
-};
--- 0LeetCodeProblems/[SolutionLeetcode]/LeetCode_Solutions-main/sqrtx/sqrtx.js
+++
@@ -1,7 +0,0 @@
-/**
- * @param {number} x
- * @return {number}
- */
-var mySqrt = function(x) {
-    return Math.floor(Math.sqrt(x))
-};
